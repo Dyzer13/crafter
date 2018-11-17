88f5d1a5723f632dd543edf11e09e7542670ab1f
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-17 13:56:26 +0100

    Update bot (1).js

--- bot (1).js
+++ bot (1).js
@@ -4,25 +4,25 @@ const client = new Discord.Client();
 
 
 client.on("ready", () => {
-console.log('By : m7md');
+console.log('BY : m7md');
 client.user.setPresence({
-  status: 'dnd',
+  status: 'ONLINE',
   game: { 
      type: 0,
-     name: 'هاي يوتيوووب',
-     details: `هاي يوتيوب`,
+     name: 'SoftNetwork',
+     details: `1kkk.`,
      url: 'http://twitch.tv/M7md_Salih',
-     state: `هاي يوتيوب`,
+     state: `SoftNetwork`,
     application_id: '377479790195769345',
      assets: {
         small_image: `377480550207717376`,
-        small_text: 'هاي يوتيوب',
+        small_text: 'SoftNetwork',
         large_image: `377480353259978752`,
-        large_text: `هاي يوتيوب` }
+        large_text: `1kkkkk` }
 
   }
     });
 });
-اقولها
 
-client.login("MzU3MjkwMjYxNDA5NDMxNTUy.DV4gXw.uUb0cO18ipmgbmsgb-6niE39vBs");
+
+client.login(process.env.TOKEN);
