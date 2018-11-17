c4e9b8fcac51020b8d3356f09970e824a84f4064
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-17 13:59:26 +0100

    Update bot (1).js

--- bot (1).js
+++ bot (1).js
@@ -1,27 +1,27 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-
-
-client.on("ready", () => {
-console.log('BY : m7md');
-client.user.setPresence({
-  status: 'ONLINE',
-  game: { 
-     type: 0,
-     name: 'SoftNetwork',
-     details: `1kkk.`,
-     url: 'http://twitch.tv/M7md_Salih',
-     state: `SoftNetwork`,
-    application_id: '377479790195769345',
-     assets: {
-        small_image: `377480550207717376`,
-        small_text: 'SoftNetwork',
-        large_image: `377480353259978752`,
-        large_text: `1kkkkk` }
-
-  }
-    });
+client.on('message', message => {
+  if (!message.content.startsWith(PREFIX)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "439187325503930369") return;
+ 
+if (message.content.startsWith(PREFIX + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/darkknite55");
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}`)
+}
+ 
+if (message.content.startsWith(PREFIX + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
+}
+if (message.content.startsWith(PREFIX + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+}
 });
 
 
