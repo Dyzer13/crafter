56e8c95cadd09d309cce19179e099eb7fac5d9e7
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-17 14:10:35 +0100

    Update node.js

--- node.js
+++ node.js
@@ -28,30 +28,11 @@ client.on('ready', () => {
   console.log(`channels! [ " ${client.channels.size} " ]`);
   console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 });
+  client.on('ready', () => { //playing
+    client.user.setGame(`SoftNetwork 2kk,`,'https://www.twitch.tv/clo_wiin');
+    client.user.setStatus('dnd')
+});
  
 
- 
-client.on('message', message => {
-  if (!message.content.startsWith(PREFIX)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== "513323053334396928") return;
- 
-if (message.content.startsWith(PREFIX + 'setstream')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/darkknite55");
-     console.log('test' + argresult);
-    message.channel.sendMessage(`Streaming: **${argresult}`)
-}
- 
-if (message.content.startsWith(PREFIX + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.sendMessage(`Username Changed To **${argresult}**`)
-  return message.reply("You Can change the username 2 times per hour");
-}
-if (message.content.startsWith(PREFIX + 'setavatar')) {
-  client.user.setAvatar(argresult);
-   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
-}
-});
 client.login("process.env.BOT_TOKEN)");
 
