34f3f50d80323224b9c6dd840ee91bc933c34e96
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-17 14:06:24 +0100

    Update node.js

--- node.js
+++ node.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const prefix = "k";
 
 client.on('message', message => {
   if (!message.content.startsWith(PREFIX)) return;
